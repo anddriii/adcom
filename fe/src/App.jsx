cd44add203a4f19bc7811eca483d77bb169b3216
rizkyzul <kyzk23@gmail.com>
2025-08-05 17:50:56 +0700

    feat: restructure App component to improve routing and organization of sections

--- fe/src/App.jsx
+++ fe/src/App.jsx
@@ -2,18 +2,39 @@ import React from 'react';
 import Navbar from './components/Navbar';
 import Hero from './components/Hero';
 import MasteringTools from './components/MasteringTools';
+import AboutSection from './components/AboutSection';
+import BenefitsSection from './components/BenefitsSection';
+import DivisionSection from './components/DivisionSection';
+import ProjectsSection from './components/ProjectsSection';
+import ProjectDetailPage from './pages/ProjectDetailPage';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
-    // <> adalah React Fragment, pembungkus tanpa membuat div baru
+  
     <>
+     <BrowserRouter>
       <Navbar />
-      <Hero />
-      <MasteringTools/>
-      
-      {/* Di sini nanti kita bisa tambahkan komponen-komponen lain 
-        sesuai urutan di desainnya.
-      */}
+      <main>
+        <Routes>
+          <Route path="/" element={
+            <>
+              <Hero />
+              <BenefitsSection />
+              <AboutSection />
+              <MasteringTools />
+              <DivisionSection />
+              <ProjectsSection />
+            </>
+          } />
+
+          {/* 2. Tambahkan Rute Baru untuk Detail Proyek */}
+          <Route path="/projects/:slug" element={<ProjectDetailPage />} />
+          
+        </Routes>
+      </main>
+      {/* <Footer /> */}
+    </BrowserRouter>
     </>
   );
 }
